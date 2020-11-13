75384ebc7cd6d8b3b9f799c3de0870b2a76f17dc
jeromeklam <jeromeklam@free.fr>
2020-11-13 15:57:05 +0100

    checkbox

--- src/features/advanced/SvgCheckbox.js
+++ src/features/advanced/SvgCheckbox.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import uuidv1 from 'uuid/v1';
 
 export default function SvgCheckbox(props) {
   let otherProps = {};
   if (props.checked || props.selected) {
     otherProps.checked = 'checked';
   }
+  const id = 'cbx-' + uuidv1();
   return (
     <div className="basic-svg-checkbox">
-      <label for="cbx" className="label-cbx" onClick={props.onChange}>
-        <input id="cbx" type="checkbox" class="invisible" {...otherProps} />
-        <div className="checkbox" style={{width: '16px', height: '16px'}}>
+      <label for={id} className="label-cbx" onClick={props.onChange}>
+        <input id={id} type="checkbox" class="invisible" {...otherProps} />
+        <div className="checkbox" style={{ width: '16px', height: '16px' }}>
           <svg width="16px" height="16px" viewBox="0 0 20 20">
-            <path d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z" />
-            <polyline points="4 11 8 15 16 6" />
+            <path id="basic-svg-checkbox-out"
+              d="M3,1 L17,1 L17,1 C18.1045695,1 19,1.8954305 19,3 L19,17 L19,17 C19,18.1045695 18.1045695,19 17,19 L3,19 L3,19 C1.8954305,19 1,18.1045695 1,17 L1,3 L1,3 C1,1.8954305 1.8954305,1 3,1 Z"
+              stroke="black" fill="white"
+            />
+            <path id="basic-svg-checkbox-inner"
+              d="M 5.334 4 L 14.666 4 L 14.666 4 C 15.403 4 16 4.597 16 5.334 L 16 14.666 L 16 14.666 C 16 15.403 15.403 16 14.666 16 L 5.334 16 L 5.334 16 C 4.597 16 4 15.403 4 14.666 L 4 5.334 L 4 5.334 C 4 4.597 4.597 4 5.334 4 Z"
+              stroke="black" fill="black"
+            />
           </svg>
         </div>
       </label>
